bcc64c5dc5f1ddb3d9ca0a967ed01b97d1e82fea
nrlsolomon <nrlsolom5@gmail.com>
2018-06-27 15:11:25 +0000

    automatically backed up by learn

---
+++ about_me.rb
@@ -0,0 +1,23 @@
+# def about_me 
+#   puts "my name is bob"
+#   puts "I am 199999 years old"
+#   puts "i live in space"
+#   puts "where do you live i bet you dont live in space"
+# end
+
+# about_me
+
+class college 
+  def initialize
+    
+    
+    
+    
+    
+    
+    
+    
+  end
+end
+
+
---
+++ arguments.rb
@@ -0,0 +1,45 @@
+# def greeting 
+  # local_greeting = "hey whats your name"
+  # your_name = "joe"
+  # my_name = "nasya"
+  # time_of_day = "morning"
+  
+  # puts "#{local_greeting} #{your_name} i'm #{my_name} how's your #{time_of_day}"
+# end
+
+# greeting ("hi", "bob", "solomon", "night",)
+
+
+# def greeting(my_name, your_name = "bob")
+#   puts "hey i,m #{my_name} how are you doing #{your_name}"
+# end
+
+# greeting("nasya","joe")
+
+
+
+
+# def disny (my_name, your_name = "jimin")
+  
+#   puts "i love #{your_name} you love #{my_name} i'm the happiest that #{my_name} can be"
+  
+# end
+
+# disny("nasya","jungkook")
+
+
+
+def bts_greeting (one_name, two_name, three_name = "jin")
+  
+puts "hi everyone i'm #{one_name} and i'm kind of the leader if this group. hi i'm #{two_name} i'm your hope and your angel. hi i'm #{three_name} nice to meet you"
+end
+
+bts_greeting("R.M", "J.hope",)
+
+
+
+
+
+
+
+
